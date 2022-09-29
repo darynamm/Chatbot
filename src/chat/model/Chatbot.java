406f74c99c27dc663316b6a88a8a3ac510a83616
darynamm <darynamanuilenko@gmail.com>
2022-09-29 09:43:37 -0600

    made it s that chabot gives the same reponse as what you type in

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -2,5 +2,18 @@ package chat.model;
 
 public class Chatbot
 {
+private String chatbotName;
+private String userName;
 
+public Chatbot(String chatbotName)
+{
+	this.chatbotName = chatbotName;
+	this.userName = "";
+}
+
+public String processText(String input) {
+	String output = input;
+	
+	return output;
+}
 }
--- src/control/controller/Controller.java
+++ src/control/controller/Controller.java
@@ -9,12 +9,28 @@ public class Controller
 
 	public Controller()
 	{
-		this.chatbot = new Chatbot();
+	this.chatbot = new Chatbot("Super smart chatbot tbh");
 		this.keyboardInput = new Scanner(System.in);
 	}
 	public void start()
 	{
+		System.out.println("Let's talk to the chatbot");
+		String text = keyboardInput.nextLine();
+		
+		while (!text.equalsIgnoreCase("shutdown "))
+		{
+			String chatbotResponse = interactWithChatbot(text);
+			System.out.println("Chatbot says: ");
+			System.out.println(chatbotResponse);
+			text = keyboardInput.nextLine();
+		}
 		keyboardInput.close();
+	}
+	private String interactWithChatbot(String userText)
+	{
+		String response = "";
+		response = chatbot.processText(userText);
+		return response;
 	}
 }
 
