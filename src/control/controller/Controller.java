7a09b85fff0fb2b0e9a7b9cec22a51b950daf62b
darynamm <darynamanuilenko@gmail.com>
2022-09-29 09:17:49 -0600

    made the chatbots first constructor and made an update to it

--- src/control/controller/Controller.java
+++ src/control/controller/Controller.java
@@ -1,6 +1,20 @@
 package control.controller;
 
+import chat.model.Chatbot;
+import java.util.Scanner;
 public class Controller
 {
+	private Chatbot chatbot;
+	private Scanner keyboardInput;
 
+	public Controller()
+	{
+		this.chatbot = new Chatbot();
+		this.keyboardInput = new Scanner(System.in);
+	}
+	public void start()
+	{
+		keyboardInput.close();
+	}
 }
+
--- src/control/controller/Runner.java
+++ src/control/controller/Runner.java
@@ -2,5 +2,9 @@ package control.controller;
 
 public class Runner
 {
-
+public static void main (String [] args)
+{
+	Controller app = new Controller();
+	app.start();
+}
 }
